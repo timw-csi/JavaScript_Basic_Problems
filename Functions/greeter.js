ed35d2191b0b5b7f35cb6fdf627e5eb69aa11640
Tim Williams <timw636@gmail.com>
2021-09-29 16:06:42 -0700

    Adding exercises from Functions chapter

---
+++ Functions/greeter.js
@@ -0,0 +1,4 @@
+let rlSync = require('readline-sync');
+let firstName = rlSync.question("What is your first name? ");
+let lastName = rlSync.question("What is your last name? ");
+console.log("Hello, " + firstName + " " + lastName + "!");
---
+++ Functions/greeter_func.js
@@ -0,0 +1,9 @@
+function getName(prompt) {
+  let rlSync = require('readline-sync');
+  let name = rlSync.question(prompt);
+  return name;
+}
+
+let firstName = getName('What is your first name? ');
+let lastName = getName('What is your last name? ');
+console.log(`Hello ${firstName} ${lastName}`);
